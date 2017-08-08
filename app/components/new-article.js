7b7c6f78237c1c586b2aaefa186964c3433e1a2c
Max Scher and Nicole Freed <max@>
2017-08-08 14:36:58 -0700

    implement addNewArticle functionality in new-article.js

--- app/components/new-article.js
+++ app/components/new-article.js
@@ -1,4 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewArticle: false,
+  actions: {
+    articleFormShow() {
+      this.set('addNewArticle', true)
+    },
+
+    saveArticle() {
+      var params = {
+        title: this.get('title') ? this.get('title') : "",
+        image: this.get('image') ? this.get('image') : "",
+        teaser: this.get('teaser') ? this.get('teaser') : "",
+        author: this.get('author') ? this.get('author') : "",
+        fulltext: this.get('fulltext') ? this.get('fulltext') : "",
+      };
+      this.set('addNewArticle', false);
+      this.sendAction('saveArticle', params);
+    }
+  }
 });
